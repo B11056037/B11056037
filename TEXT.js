31b3f01272b09d43aa98e1b444695f45d88bf058
B110560337 <ssss5566449@gmail.com>
2022-03-27 00:36:56 +0800

    text

---
+++ TEXT.js
@@ -0,0 +1,39 @@
+var a = 0xFFF
+var b = 0xFAB
+var X = 5
+var Y = 10
+document.write("0xFFF - 0xFAB =" + a - b);
+document.write("<br>");
+document.write("2 / 3.0 =" + 2 / 3.0);
+document.write("<br>");
+document.write("True /0 =" + true / 0);
+document.write("<br>");
+document.write("False / 0 = " + false / 0);
+document.write("<br>");
+document.write("12.3 * 10 % 5 = " + 12.3 * 10 % 5);
+document.write("<br>");
+document.write("\'a\' >\'Z\' = ");
+document.write('a' > 'Z');
+document.write("<br>");
+document.write("~0x0005 = " + ~0x0005);
+document.write("<br>");
+document.write("\'abc\' + 88 == \'ABC\' + 88 = ");
+document.write('abc' + 88 == 'ABC' + 88);
+document.write("<br>");
+document.write("50 > 30 ^ 70 > 100 = ");
+document.write(50 > 30 ^ 70 > 100);
+document.write("<br>");
+document.write("3|5 = ")
+document.write(3 | 7);
+document.write("<br>");
+document.write("8 > 7 true or false = ");
+document.write(8 > 7);
+document.write("<br>");
+document.write("128>>3 = ");
+document.write(128 >> 3);
+document.write("<br>");
+document.write("X=5 , ++X = ");
+document.write(++X);
+document.write("<br>");
+document.write("Y=10,Y-- = ");
+document.write(--Y);
