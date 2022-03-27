cf97d3ff7d6bbedf02f2c8c0e80e0d63d48f37ba
B11056037 <ssss5566449@gmail.com>
2022-03-27 12:04:56 +0800

    功課,使用FOR

--- 功課在這裡/B11056037_For.js
+++ 功課在這裡/B11056037_For.js
@@ -1,6 +1,6 @@
 var j = 8
 document.write("<table  id=tableCaption>");
-document.write("<caption><h1>MIS快餐店價格表</h1></caption>");
+document.write("<caption><h1>MIS快餐店的價格表</h1></caption>");
 document.write("<tr id=tableTitle><th id=none></th>");
 for (var i = 5; i >= 1; i--) {
     document.write("<th id=tableTitle>" + i + "號餐" + j + "元")
